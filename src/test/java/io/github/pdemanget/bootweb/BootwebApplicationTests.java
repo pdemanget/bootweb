686f987980844d0cd10676ec14a2dcb77421dadf
pdemanget <pdemanget@gmail.com>
2019-07-28 18:52:56 +0200

    start.spring.io init

---
+++ src/test/java/io/github/pdemanget/bootweb/BootwebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pdemanget.bootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
